1df546e5d2174b6d5fc7b636d1a1916286ffa2c1
newuhe <2383766952@qq.com>
2017-08-17 07:13:13 +0800

    Feed data into model

--- assignment1.py
+++ assignment1.py
@@ -1,16 +1,21 @@
 import tensorflow as tf
 import numpy as np
-import utils
+import os
 import zipfile
+import random
 from collections import Counter
 
 
-VOCAB_SIZE = 50000
+VOCAB_SIZE = 10000
 BATCH_SIZE = 128
-EMBED_SIZE = 128
-SKIP_STEP=2000
+EMBED_SIZE = 300
+DISPLAY_STEP=1000
+TRAIN_STEPS=10000
+NUM_SAMPLED = 64    # Number of negative examples to sample.
+SKIP_WINDOW=3
 LEARNING_RATE = 1.0
 
+#process data
 def read_data(file_path):
     """ Read data into a list of tokens 
     There should be 17,005,207 tokens
@@ -20,13 +25,13 @@ def read_data(file_path):
         # tf.compat.as_str() converts the input into the string
     return words
 
-def build_vocab(words, vocab_size):
+def build_vocabulary(words, vocab_size):
     """ Build vocabulary of VOCAB_SIZE most frequent words """
     dictionary = dict()
     count = [('UNK', -1)]
     count.extend(Counter(words).most_common(vocab_size - 1))
     index = 0
-    utils.make_dir('processed')
+    os.makedirs('processed')
     with open('processed/vocab_1000.tsv', "w") as f:
         for word, _ in count:
             dictionary[word] = index
@@ -36,8 +41,40 @@ def build_vocab(words, vocab_size):
     index_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
     return dictionary, index_dictionary
 
+def convert_words_to_index(words, dictionary):
+    """ Replace each word in the dataset with its index in the dictionary """
+    return [dictionary[word] if word in dictionary else 0 for word in words]
 
-def word2vec():
+def generate_sample(index_words, context_window_size):
+    """ Form training pairs according to the skip-gram model. """
+    for index, center in enumerate(index_words):
+        context = random.randint(1, context_window_size)
+        # get a random target before the center word
+        for target in index_words[max(0, index - context): index]:
+            yield center, target
+        # get a random target after the center wrod
+        for target in index_words[index + 1: index + context + 1]:
+            yield center, target
+
+def get_batch(iterator, batch_size):
+    """ Group a numerical stream into batches and yield them as Numpy arrays. """
+    while True:
+        center_batch = np.zeros(batch_size, dtype=np.int32)
+        target_batch = np.zeros([batch_size, 1])
+        for index in range(batch_size):
+            center_batch[index], target_batch[index] = next(iterator)
+        yield center_batch, target_batch
+        
+def process_data(vocab_size, batch_size, skip_window):
+    words = read_data("E:/MachineLearning/CS20SI/text8.zip")
+    dictionary, _ = build_vocabulary(words, vocab_size)
+    index_words = convert_words_to_index(words, dictionary)
+    del words # to save memory
+    single_gen = generate_sample(index_words, skip_window)
+    return get_batch(single_gen, batch_size)
+
+#build a model
+def word2vec(batch_gen):
 # Phase1: assemble your graph
     # Step 1: define the placeholders for input and output
     with tf.name_scope("data"):
@@ -71,14 +108,13 @@ def word2vec():
         writer = tf.summary.FileWriter('./graphs/', sess.graph)
         total_loss=0
         
-        for index in range():
-            centers=
-            targets=
+        for index in range(TRAIN_STEPS):
+            centers,targets=next(batch_gen)
             batch_loss, _ = sess.run([loss, optimizer], 
                                     feed_dict={center_words: centers, target_words: targets})
             total_loss += batch_loss
-            if (index + 1) % SKIP_STEP == 0:
-                print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
+            if (index + 1) % DISPLAY_STEP == 0:
+                print('Average loss at step {}: {:5.1f}'.format(index, total_loss / DISPLAY_STEP))
                 total_loss = 0.0
                 
         writer.close()
