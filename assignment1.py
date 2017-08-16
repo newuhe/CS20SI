8f61e2441f45195e05d8be5795f1443eb661ddd2
newuhe <2383766952@qq.com>
2017-08-16 13:32:57 +0800

    Unfinished

--- assignment1.py
+++ assignment1.py
@@ -1,23 +1,57 @@
 import tensorflow as tf
+import numpy as np
 
+VOCAB_SIZE = 50000
+BATCH_SIZE = 128
+EMBED_SIZE = 128
+SKIP_STEP=2000
+LEARNING_RATE = 1.0
 
 def word2vec():
 # Phase1: assemble your graph
     # Step 1: define the placeholders for input and output
     with tf.name_scope("data"):
-        
+        center_words = tf.placeholder(tf.int32, shape=[BATCH_SIZE], name='center_words')
+        target_words = tf.placeholder(tf.int32, shape=[BATCH_SIZE, 1], name='target_words')
     # Step 2: define weights
     with tf.name_scope("weight"):
-        
+        embed_matrix = tf.Variable(tf.random_uniform([VOCAB_SIZE, EMBED_SIZE], -1.0, 1.0), 
+                            name='embed_matrix')
     # Step 3: define inference model        
     with tf.name_scope("loss"):
-        
+        embed = tf.nn.embedding_lookup(embed_matrix, center_words, name='embed')
+        nce_weight = tf.Variable(tf.truncated_normal([VOCAB_SIZE, EMBED_SIZE],
+                                                    stddev=1.0 / (EMBED_SIZE ** 0.5)), 
+                                                    name='nce_weight')
+        nce_bias = tf.Variable(tf.zeros([VOCAB_SIZE]), name='nce_bias')
         # Step 4: define the loss function
-    
+        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weight, 
+                                            biases=nce_bias, 
+                                            labels=target_words, 
+                                            inputs=embed, 
+                                            num_sampled=NUM_SAMPLED, 
+                                            num_classes=VOCAB_SIZE), name='loss')
+
     # Step 5: define the optimizer
+    optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
     
 # Phase2: ececute the computation
     with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        writer = tf.summary.FileWriter('./graphs/', sess.graph)
+        total_loss=0
+        
+        for index in range():
+            centers=
+            targets=
+            batch_loss, _ = sess.run([loss, optimizer], 
+                                    feed_dict={center_words: centers, target_words: targets})
+            total_loss += batch_loss
+            if (index + 1) % SKIP_STEP == 0:
+                print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
+                total_loss = 0.0
+                
+        writer.close()
 
 def main():
     batch_gen = process_data(VOCAB_SIZE, BATCH_SIZE, SKIP_WINDOW)
