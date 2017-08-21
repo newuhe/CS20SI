643824a8db607001d7f1d9e18acbe389e9b613d0
newuhe <2383766952@qq.com>
2017-08-21 09:28:43 +0800

    Visualize word embedding

--- assignment1.py
+++ assignment1.py
@@ -119,6 +119,30 @@ def word2vec(batch_gen):
                 total_loss = 0.0
                 
         writer.close()
+        
+# Phase3: visulize       
+        # code to visualize the embeddings. uncomment the below to visualize embeddings
+        # run "'tensorboard --logdir='processed'" to see the embeddings
+        final_embed_matrix = sess.run(embed_matrix) #result of sess.run is constant rather than variable
+        
+        # # it has to variable. constants don't work here. you can't reuse model.embed_matrix
+        embedding_var = tf.Variable(final_embed_matrix[:1000], name='embedding')
+        sess.run(embedding_var.initializer)
+
+        config = projector.ProjectorConfig()
+        summary_writer = tf.summary.FileWriter('processed')
+
+        # # add embedding to the config file
+        embedding = config.embeddings.add()
+        embedding.tensor_name = embedding_var.name
+        
+        # # link this tensor to its metadata file, in this case the first 500 words of vocab
+        embedding.metadata_path = 'processed/vocab_1000.tsv'
+
+        # # saves a configuration file that TensorBoard will read during startup.
+        projector.visualize_embeddings(summary_writer, config)
+        saver_embed = tf.train.Saver([embedding_var])
+        saver_embed.save(sess, 'processed/model3.ckpt', 1)
 
 def main():
     batch_gen = process_data(VOCAB_SIZE, BATCH_SIZE, SKIP_WINDOW)
